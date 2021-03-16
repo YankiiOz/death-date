b9cedbbb08fcf24fb16e005c6322351a571dab74
YankiiOz <yankii-blimm@mail.ru>
2021-03-16 10:30:15 +0300

    queation block and loading added

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
 
-let button = document.querySelector(".test-button");
+let buttons = document.getElementsByClassName("test-button");
 let blockFirst = document.querySelector(".question-1");
 let blockSecond = document.querySelector(".question-2");
 let blockThird = document.querySelector(".question-3");
 let blockFourth = document.querySelector(".question-4");
+let blockFifth = document.querySelector(".question-5")
 
-button.onclick = function() {
-    if(blockFirst.classList.contains('.block')) {
-        blockFirst.classList.remove('.block');
-        blockSecond.classList.add('.block');
+for(let i=0;i<buttons.length; i++) {
+    buttons[i].onclick = function() {
+        if(blockFirst.classList.contains("block")) {
+            blockFirst.classList.remove("block");
+            blockSecond.classList.add("block");
+        } else if(blockSecond.classList.contains("block")) {
+            blockSecond.classList.remove("block");
+            blockThird.classList.add("block");
+        } else if(blockThird.classList.contains("block")) {
+            blockThird.classList.remove("block");
+            blockFourth.classList.add("block");
+        } else if(blockFourth.classList.contains("block")) {
+            blockFourth.classList.remove("block");
+            blockFifth.classList.add("block");
+        };
     }
+}
 
-};
+function progress () {
+    let elem = document.getElementById("upper");
+    let percent = document.querySelector(".recording-message__percent");
+    width = 0;
+    id = setInterval(progressStatus, 100)
+
+    function progressStatus () {
+        if(width>=100) {
+            clearInterval(id)
+        } else {
+            width++;
+            elem.style.width = width +"%";
+            percent.innerHTML = width * 1 + "%";
+        }
+    }
+}
+
+progress();
